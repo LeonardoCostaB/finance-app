7b5a61eaa7f51fb5bb54c9c686e9e97a4229054c
Leonardo Costa <leonardo.costa.b@outlook.com>
2024-06-30 12:59:47 -0300

    fix: apollo client uri

--- src/graphql/apollo-client.ts
+++ src/graphql/apollo-client.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const apolloClient = new ApolloClient({
-   uri: `${process.env.MAIN_URL}/api/graphql`,
+   uri: `${process.env.MAIN_URL as string}/api/graphql`,
    cache: new InMemoryCache(),
 });
