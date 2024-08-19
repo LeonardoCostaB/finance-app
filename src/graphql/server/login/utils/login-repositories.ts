7b35064af9e6a8458f37547f317385982e60479e
Leonardo Costa <leonardo.costa.b@outlook.com>
2024-08-19 15:26:34 -0300

    fix: increases token expiration time

--- src/graphql/server/login/utils/login-repositories.ts
+++ src/graphql/server/login/utils/login-repositories.ts
@@ -2,6 +2,6 @@ import jwt from 'jsonwebtoken';
 
 export function createJwtToken(payload: { id: string }) {
    return jwt.sign(payload, process.env.JWL_SECRET_KEY as string, {
-      expiresIn: '1d',
+      expiresIn: '7d',
    });
 }
